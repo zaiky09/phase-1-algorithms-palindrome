a9cc4e5a2c0a547a45953ff0a71ef0291f82de9d
zaiky09 <zamilmozamil@outlook.com>
2022-12-13 06:20:27 +0300

    done

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString = reverseLetters.join('')
+
+  if (word == reverseString) {
+    return true
+  } else {
+    return false
+  }
 }
 
-/* 
-  Add your pseudocode here
-*/
+//Pseudo code
 
-/*
-  Add written explanation of your solution here
-*/
+@@ -13,6 +23,12 @@ function isPalindrome(word) {}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('girl'));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("tonia"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
